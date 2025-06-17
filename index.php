c4cfb7a7c21b9a6b624fbcfc538c56831cda74a4
abdulswabul26 <abdulswabul26>
2025-06-17 11:06:44 +0300

    header modified

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Divine</title>
+
+    <!-- CSS Links -->
+     <link rel="stylesheet" href="css/reset.css">
+     <link rel="stylesheet" href="css/header.css">
+</head>
+<body>
+    <header>
+    <div><a href="index.php"><img class="logo" src="assets/logo/log.png" alt="logo"></a></div>
+
+    <button class="menu-toggle" onclick="toggleMenu()">☰</button>
+
+    <ul id="navList">
+        <li><a href="index.php">Home</a></li>
+        <li>About</li>
+        <li><a href="">News & Events</a></li>
+        <li><a href="">Admission</a></li>
+        <li><a href="">Contact</a></li>
+    </ul>
+
+    <a href="#" class="enroll">Enroll Now</a>
+</header>
+
+
+        <div id="ham">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        
+    </header>
+    
+</body>
+</html>
